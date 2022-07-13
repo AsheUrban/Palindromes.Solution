fb175046cb1ea72a8039bffec247d26f0e591114
Ashe Urban <ashe@goldentongue.com>
2022-07-13 10:59:57 -0700

    ADD comments to Program.cs. :

--- Palindromes/Program.cs
+++ Palindromes/Program.cs
@@ -8,14 +8,17 @@ namespace PChecker.Models
   {
     public static void Main()
     {
-      Palindrome userInput1 = new Palindrome();
-      // Palindrome userInput2 = new Palindrome();
 
       Console.WriteLine("Enter a word to learn if it is a palindrome: ");
-      string userInputWord = Console.ReadLine();
+      string userInputWord = Console.ReadLine(); // Do we need to pass an argument here?
+
+      Palindrome userInput1 = new Palindrome(userInput);
+      // Palindrome userInput2 = new Palindrome();
 
       Console.WriteLine(userInput);
       Console.WriteLine(revArray);
+
+      // Need to call IsPalindrome
 
     }
   }
